fad501142d55f1a77764643be7ee00f78a14615f
beauxq <beauxq@users.noreply.github.com>
2020-03-26 20:21:29 -0700

    divide subroutine

--- bern.py
+++ bern.py
@@ -64,10 +64,84 @@ def main():
     computer.ram.memory[8] = a.m(ASM.OUT, 0)
     computer.ram.memory[9] = a.m(ASM.HLT, 0)
 
-    computer.clock.go(1000)
+    computer.clock.go()
 
     computer.control.reset()
 
+    print(" divide subroutine")
+    divide_subroutine = [
+        a.m(ASM.LDI, 1),
+        a.m(ASM.STA, 2254),
+        a.m(ASM.LDA, 2250),
+        a.m(ASM.ADD, 4000),
+        a.m(ASM.JC , 2206),
+        a.m(ASM.JMP, 2211),
+        a.m(ASM.LDA, 4002),
+        a.m(ASM.STA, 2254),
+        a.m(ASM.LDI, 0),
+        a.m(ASM.SUB, 2250),
+        a.m(ASM.STA, 2250),
+        a.m(ASM.LDI, 0),
+        a.m(ASM.STA, 2255),
+        a.m(ASM.LDA, 2250),
+        a.m(ASM.SUB, 2251),
+        a.m(ASM.ADD, 4000),
+        a.m(ASM.JC , 2224),
+        a.m(ASM.LDA, 2250),
+        a.m(ASM.SUB, 2251),
+        a.m(ASM.STA, 2250),
+        a.m(ASM.LDA, 2255),
+        a.m(ASM.ADD, 4001),
+        a.m(ASM.STA, 2255),
+        a.m(ASM.JMP, 2213),
+        a.m(ASM.LDA, 2254),
+        a.m(ASM.STA, 2033),
+        a.m(ASM.LDA, 2255),
+        a.m(ASM.STA, 2034),
+        a.m(ASM.LDI, 2231),
+        a.m(ASM.STA, 2035),
+        a.m(ASM.JMP, 2000),
+        a.m(ASM.LDA, 2036),
+        a.m(ASM.STA, 2253),
+        a.m(ASM.JI , 2252)
+    ]
+    """
+    integer division
+    parameters:
+    2250 a
+    2251 b  must be positive
+    2252 pcreturnlocation
+
+    output:
+    2253 result
+
+    working variables:
+    2254 a_sign
+    2255 count
+    """
+
+    address = 2200
+    for instruction in divide_subroutine:
+        computer.ram.memory[address] = instruction
+        address += 1
+
+    # use the divide subroutine
+    computer.ram.memory[0] = a.m(ASM.LDI, 1764)
+    computer.ram.memory[1] = a.m(ASM.STA, 2250)  # a operand
+    computer.ram.memory[2] = a.m(ASM.LDI, 42)
+    computer.ram.memory[3] = a.m(ASM.STA, 2251)  # b operand
+    computer.ram.memory[4] = a.m(ASM.LDI, 7)
+    computer.ram.memory[5] = a.m(ASM.STA, 2252)  # return program counter
+    computer.ram.memory[6] = a.m(ASM.JMP, 2200)  # add fractions subroutine
+    computer.ram.memory[7] = a.m(ASM.LDA, 2253)  # result numer
+    computer.ram.memory[8] = a.m(ASM.OUT, 0)
+    computer.ram.memory[9] = a.m(ASM.HLT, 0)
+
+    computer.clock.go()
+
+    computer.control.reset()
+
+    print(" add fractions subroutine")
     add_fractions_subroutine = [
         a.m(ASM.LDA, 2140),
         a.m(ASM.STA, 2147),
