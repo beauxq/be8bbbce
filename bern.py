25232ed497caf5514366afcb2c75964b0ca706f4
beauxq <beauxq@users.noreply.github.com>
2020-03-26 20:41:08 -0700

    gcd subroutine

--- bern.py
+++ bern.py
@@ -132,8 +132,56 @@ def main():
     computer.ram.memory[3] = a.m(ASM.STA, 2251)  # b operand
     computer.ram.memory[4] = a.m(ASM.LDI, 7)
     computer.ram.memory[5] = a.m(ASM.STA, 2252)  # return program counter
-    computer.ram.memory[6] = a.m(ASM.JMP, 2200)  # add fractions subroutine
-    computer.ram.memory[7] = a.m(ASM.LDA, 2253)  # result numer
+    computer.ram.memory[6] = a.m(ASM.JMP, 2200)  # divide subroutine
+    computer.ram.memory[7] = a.m(ASM.LDA, 2253)  # result
+    computer.ram.memory[8] = a.m(ASM.OUT, 0)
+    computer.ram.memory[9] = a.m(ASM.HLT, 0)
+
+    computer.clock.go()
+
+    computer.control.reset()
+
+    print(" gcd subroutine")
+    gcd_subroutine = [
+        a.m(ASM.LDA, 2350),
+        a.m(ASM.SUB, 2351),
+        a.m(ASM.JZ , 2313),
+        a.m(ASM.ADD, 4000),
+        a.m(ASM.JC , 2309),
+        a.m(ASM.LDA, 2350),
+        a.m(ASM.SUB, 2351),
+        a.m(ASM.STA, 2350),
+        a.m(ASM.JMP, 2300),
+        a.m(ASM.LDA, 2351),
+        a.m(ASM.SUB, 2350),
+        a.m(ASM.STA, 2351),
+        a.m(ASM.JMP, 2300),
+        a.m(ASM.JI , 2352)
+    ]
+    """
+    parameters:
+    2350 a  must be positive
+    2351 b  must be positive
+    2352 pc return location
+
+    output:
+    2350 result
+    """
+
+    address = 2300
+    for instruction in gcd_subroutine:
+        computer.ram.memory[address] = instruction
+        address += 1
+
+    # use the gcd subroutine
+    computer.ram.memory[0] = a.m(ASM.LDI, 210)
+    computer.ram.memory[1] = a.m(ASM.STA, 2350)  # a operand
+    computer.ram.memory[2] = a.m(ASM.LDI, 546)
+    computer.ram.memory[3] = a.m(ASM.STA, 2351)  # b operand
+    computer.ram.memory[4] = a.m(ASM.LDI, 7)
+    computer.ram.memory[5] = a.m(ASM.STA, 2352)  # return program counter
+    computer.ram.memory[6] = a.m(ASM.JMP, 2300)  # gcd subroutine
+    computer.ram.memory[7] = a.m(ASM.LDA, 2350)  # result
     computer.ram.memory[8] = a.m(ASM.OUT, 0)
     computer.ram.memory[9] = a.m(ASM.HLT, 0)
 
