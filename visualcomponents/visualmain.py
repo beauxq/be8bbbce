6ba64cfcd7481042fb73ffc3973d6b30ca4bd32f
beauxq <beauxq@users.noreply.github.com>
2020-03-28 07:52:55 -0700

    rework vertical labels

--- visualcomponents/visualmain.py
+++ visualcomponents/visualmain.py
@@ -4,7 +4,7 @@ from visualcomponents.ramreader import RamReader
 from visualcomponents.irreader import IRReader
 from visualcomponents.signalwatcher import SignalWatcher
 import pygame
-from typing import Tuple
+from typing import Tuple, List, Union
 
 MAX_CLOCK_HZ = 250
 
@@ -119,16 +119,11 @@ class VisualMain:
                         .76, .4, (1, 0, 0), "Sum Register")
         self.draw_value(self.computer.reg_b, bit_count,
                         .85, .55, (1, 0, 0), '"B" Register')
-        # a lot of math to get the position right for these labels
-        self.draw_value(self.sw, 0,
-                        .003125 * bit_count + .525 +
-                        (self.screen.get_width() - 700) / 50000,
-                        -.001875 * bit_count + .95,
-                        (1, 1, .8),
-                        " L I I O O I I O O U I I E O   I ")
         self.draw_value(self.sw, 16,
                         .6, .9, (0, 0, 1),
-                        " H M R R I I A A E S B O C C J F ")
+                        ["HLT", "MI", "RI", "RO", "IO", "II", "AI", "AO",
+                         "ΣO", "SU", "BI", "OI", "CE", "CO", "J", "FI"],
+                        3)
 
         # controls
         # clock stuff
@@ -141,27 +136,27 @@ class VisualMain:
         pygame.draw.rect(self.screen, color, pygame.Rect(x, y, size * 4, size))
         # pause
         text = self.button_font.render('P', True, (10, 0, 120))
-        textRect = text.get_rect()
-        textRect.center = (x + (size * 3.5), y + self.led_size())
-        self.screen.blit(text, textRect)
+        text_rect = text.get_rect()
+        text_rect.center = (x + (size * 3.5), y + self.led_size())
+        self.screen.blit(text, text_rect)
         # clock tick
         text = self.button_font.render("C", True, (0, 150, 0))
-        textRect = text.get_rect()
-        textRect.center = (x + (size * 2.5), y + self.led_size())
-        self.screen.blit(text, textRect)
+        text_rect = text.get_rect()
+        text_rect.center = (x + (size * 2.5), y + self.led_size())
+        self.screen.blit(text, text_rect)
         # speed up
         max_fps = MAX_CLOCK_HZ * 2
         sqrt_max_fps = max_fps ** .5
         color = int(252 * (self.fps ** .5) / sqrt_max_fps)
         text = self.button_font.render("]", True, (color, color, 140))
-        textRect = text.get_rect()
-        textRect.center = (x + (size * 1.5), y + self.led_size())
-        self.screen.blit(text, textRect)
+        text_rect = text.get_rect()
+        text_rect.center = (x + (size * 1.5), y + self.led_size())
+        self.screen.blit(text, text_rect)
         # slow down
         text = self.button_font.render("[", True, (color, color, 120))
-        textRect = text.get_rect()
-        textRect.center = (x + (size * 0.5), y + self.led_size())
-        self.screen.blit(text, textRect)
+        text_rect = text.get_rect()
+        text_rect.center = (x + (size * 0.5), y + self.led_size())
+        self.screen.blit(text, text_rect)
 
         # reset button
         color = (120, 120, 170)
@@ -171,9 +166,10 @@ class VisualMain:
         pygame.draw.rect(self.screen, color, pygame.Rect(x, y, size, size))
         # r
         text = self.button_font.render('R', True, (80, 80, 0))
-        textRect = text.get_rect()
-        textRect.center = (x + (size * 0.5), y + (size * 0.5))
-        self.screen.blit(text, textRect)
+        text_rect = text.get_rect()
+        text_rect.center = (x + (size * 0.5), y + (size * 0.5))
+        self.screen.blit(text, text_rect)
+
 
         # output
         color = (50, 0, 0)
@@ -189,7 +185,7 @@ class VisualMain:
 
     def led_size(self):
         return (min(self.screen.get_width(), self.screen.get_height()) /
-                ((self.computer.bit_count * 3.5) + 4)
+                ((self.computer.bit_count * 2.5) + 12)
                )
 
     def draw_value(self,
@@ -198,7 +194,8 @@ class VisualMain:
                    x_portion: float,
                    y_portion: float,
                    led_color: Tuple[float],
-                   label_text: str):
+                   label_text: Union[str, List[str]],
+                   vertical_label_height=0):
         """
         component is anything with value attribute - duck typing
         bit_count is number of LEDs to show
@@ -218,20 +215,38 @@ class VisualMain:
         value = component.value
         radius = bit_size / 2
         circle_y = y + radius
+        if vertical_label_height:
+            pygame.draw.rect(self.screen, (240, 240, 240),
+                             pygame.Rect(x, y + bit_size, width,
+                                         bit_size * vertical_label_height))
         for bit in range(bit_count):
-            brightness = 255 if (value % 2) else 100
+            brightness = 255 if (value % 2) else 80
             color = tuple((c * brightness for c in led_color))
             circle_x = x + width - (bit_size * bit + radius)
             pygame.draw.circle(self.screen,
                                color,
                                (int(circle_x), int(circle_y)),
                                int(radius))
+            if vertical_label_height:
+                this_label = label_text[-(1+bit)]
+                label_y = circle_y
+                for letter in this_label:
+                    label_y += bit_size
+                    text = self.label_font.render(letter, True,
+                                                  (16, 16, 16))
+                    text_rect = text.get_rect()
+                    text_rect.center = (circle_x, label_y)
+                    self.screen.blit(text, text_rect)
             value >>= 1
+        if (not vertical_label_height) and label_text:
         font = self.label_font
         if label_text.startswith(" "):
             font = self.signal_font
         text = font.render(label_text, True,
                            (16, 16, 16), (240, 240, 240))
-        textRect = text.get_rect()
-        textRect.center = (x + (width / 2), y + (bit_size * 1.6))
-        self.screen.blit(text, textRect)
+            text_rect = text.get_rect()
+            x_text = x + (width / 2)
+            if isinstance(component, IRReader):
+                x_text = x + (width / 3)
+            text_rect.center = (x_text, y + (bit_size * 1.6))
+            self.screen.blit(text, text_rect)
