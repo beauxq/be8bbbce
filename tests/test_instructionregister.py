9780bef32ed9fbd2a79599928e0bccca11e7e077
Doug Hoskisson <doughoskisson@novuslabs.com>
2021-10-15 15:01:33 -0700

    test instruction register

---
+++ tests/test_instructionregister.py
@@ -0,0 +1,24 @@
+from components.instructionregister import InstructionRegister
+from components.signals import Signals
+from components.bus import Bus
+
+
+def test_instructionregister() -> None:
+    signals = Signals()
+    bus = Bus()
+    ir = InstructionRegister(signals, bus, 4)
+
+    bus.value = (14 << ir.address_length) | 5
+    signals.signal(Signals.INSTR_IN, 1)
+    signals.signal(Signals.CLOCK, 1)
+    signals.signal(Signals.CLOCK, 0)
+    signals.signal(Signals.INSTR_IN, 0)
+    
+    assert ir.value == (14 << ir.address_length) | 5
+
+    signals.signal(Signals.INSTR_OUT, 1)
+    signals.signal(Signals.CLOCK, 1)
+    signals.signal(Signals.CLOCK, 0)
+    signals.signal(Signals.INSTR_OUT, 0)
+
+    assert bus.value == 5
