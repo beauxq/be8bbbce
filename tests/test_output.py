1f94f6a4e5140bd45eb65addb907a59ce99cdc27
beauxq <beauxq@users.noreply.github.com>
2021-10-17 19:33:59 -0700

    test output

---
+++ tests/test_output.py
@@ -0,0 +1,40 @@
+from components.output import Output
+from components.signals import Signals
+from components.bus import Bus
+import _pytest.capture
+
+
+def test_u2s() -> None:
+    signals = Signals()
+    bus = Bus()
+    out = Output(signals, bus, 8)
+
+    for i in range(128):
+        assert out.unsigned_to_signed(i) == i
+        assert out.unsigned_to_signed(i + 128) == i - 128
+
+def test_output(capsys: _pytest.capture.CaptureFixture[str]) -> None:
+    signals = Signals()
+    bus = Bus()
+    Output(signals, bus, 8)
+
+    bus.value = 77
+    signals.signal(Signals.OUTPUT_IN, 1)
+    signals.signal(Signals.CLOCK, 1)
+    signals.signal(Signals.CLOCK, 0)
+    signals.signal(Signals.OUTPUT_IN, 0)
+    printed = capsys.readouterr()
+
+    assert "77" in printed.out
+
+    bus.value = 130
+    signals.signal(Signals.OUTPUT_IN, 1)
+    signals.signal(Signals.CLOCK, 1)
+    signals.signal(Signals.CLOCK, 0)
+    signals.signal(Signals.OUTPUT_IN, 0)
+    printed = capsys.readouterr()
+
+    assert "130" in printed.out
+    # two's complement
+    assert "-126" in printed.out
+    
