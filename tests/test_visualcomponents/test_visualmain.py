9ed6548ac2d88b5cea322fa925f52c4042a026e0
beauxq <beauxq@users.noreply.github.com>
2021-10-23 16:08:06 -0700

    refactor test visualmain

--- tests/test_visualcomponents/test_visualmain.py
+++ tests/test_visualcomponents/test_visualmain.py
@@ -1,4 +1,9 @@
-from typing import Any, Dict, Iterator
+"""
+race conditions could cause these tests to fail
+because VisualMain and Computer are not programmed to be thread safe
+"""
+
+from typing import Any, Callable, Dict, Iterator, Tuple
 import pygame
 from be8bbbce.computer import Computer
 from be8bbbce.visualcomponents.visualmain import VisualMain
@@ -8,162 +13,191 @@ from time import sleep
 import pytest
 
 
+FixType = Tuple[
+    VisualMain,
+    Callable[[int, Dict[str, Any]], None],
+    Any  # no good solution to this
+    # https://stackoverflow.com/questions/68386130/how-to-type-hint-a-callable-of-a-function-with-default-arguments
+]
+
+
 @pytest.fixture
-def vm(patched_pygame_event: PatchedEvent) -> Iterator[VisualMain]:
+def vm_enq_press(patched_pygame_event: PatchedEvent) -> Iterator[FixType]:
     c = Computer(4, True)
     vm = VisualMain(c)
+
+    def enq(event_type: int, attrs: Dict[str, Any]) -> None:
+        """ enqueue an event and wait for the event to be processed """
+        patched_pygame_event.enqueue(patched_pygame_event.Event(event_type, attrs))
+        sleep(0.05)
+
+    def press_key(key: int, hold: bool = False) -> None:
+        """ `hold` simulates holding down the key for a repeated keydown signal """
+        enq(pygame.KEYDOWN, {"key": key})
+        if hold:
+            enq(pygame.KEYDOWN, {"key": key})
+        enq(pygame.KEYUP, {"key": key})
+
     run_thread = threading.Thread(None, lambda: vm.run())
     run_thread.start()
-    yield vm
+    yield vm, enq, press_key
     patched_pygame_event.enqueue(patched_pygame_event.Event(pygame.QUIT, {}))
     run_thread.join(3)
 
 
-def test_visualmain(patched_pygame_event: PatchedEvent) -> None:
-    """
-    race conditions could cause this test to fail
-    because VisualMain and Computer are not programmed to be thread safe
-    """
-    c = Computer(4, True)
-    vm = VisualMain(c)
+def test_initialization(vm_enq_press: FixType) -> None:
+    vm, _, _ = vm_enq_press
 
     assert vm.paused
     assert vm.fps > 22
 
-    run_thread = threading.Thread(None, lambda: vm.run())
-    run_thread.start()
 
-    try:
-        def enq(event_type: int, attrs: Dict[str, Any]) -> None:
-            """ enqueue an event and wait for the event to be processed """
-            patched_pygame_event.enqueue(patched_pygame_event.Event(event_type, attrs))
-            sleep(0.05)
+def test_resize(vm_enq_press: FixType) -> None:
+    vm, enq, _ = vm_enq_press
 
-        def press_key(key: int, hold: bool = False) -> None:
-            """ `hold` simulates holding down the key for a repeated keydown signal """
-            enq(pygame.KEYDOWN, {"key": key})
-            if hold:
-                enq(pygame.KEYDOWN, {"key": key})
-            enq(pygame.KEYUP, {"key": key})
+    enq(pygame.VIDEORESIZE, {"w": 300, "h": 200})
+    small_led_size = vm.led_size
+    small_font_size = vm.button_font.get_height()
+    enq(pygame.VIDEORESIZE, {"w": 400, "h": 300})
 
-        enq(pygame.VIDEORESIZE, {"w": 300, "h": 200})
-        small_led_size = vm.led_size
-        small_font_size = vm.button_font.get_height()
-        enq(pygame.VIDEORESIZE, {"w": 400, "h": 300})
+    assert vm.led_size > small_led_size
+    assert vm.button_font.get_height() > small_font_size
 
-        assert vm.led_size > small_led_size
-        assert vm.button_font.get_height() > small_font_size
 
-        press_key(pygame.K_p)
+def test_pause(vm_enq_press: FixType) -> None:
+    vm, _, press_key = vm_enq_press
 
-        assert not vm.paused
+    press_key(pygame.K_p)
 
-        press_key(pygame.K_c)
+    assert not vm.paused
 
-        assert not vm.paused
+    press_key(pygame.K_c)
 
-        press_key(pygame.K_SPACE)
+    assert not vm.paused
 
-        assert vm.paused
+    press_key(pygame.K_SPACE)
 
-        press_key(pygame.K_SPACE, True)
+    assert vm.paused
 
-        assert not vm.paused
+    press_key(pygame.K_SPACE, True)
 
-        start_0 = vm.clock_count
-        sleep(0.125)
-        end_0 = vm.clock_count
+    assert not vm.paused
 
-        assert end_0 - start_0 > 3
 
-        for _ in range(2):
-            press_key(pygame.K_RIGHTBRACKET, True)
-        start_1 = vm.clock_count
-        sleep(0.125)
-        end_1 = vm.clock_count
+def test_clock_speed(vm_enq_press: FixType) -> None:
+    vm, _, press_key = vm_enq_press
 
-        assert end_1 - start_1 > 2.125 * (end_0 - start_0)
+    press_key(pygame.K_p)
 
-        for _ in range(4):
-            press_key(pygame.K_LEFTBRACKET, True)
-        start_2 = vm.clock_count
-        sleep(0.125)
-        end_2 = vm.clock_count
+    start_0 = vm.clock_count
+    sleep(0.125)
+    end_0 = vm.clock_count
 
-        assert end_2 - start_2 < 0.8 * (end_0 - start_0)
+    assert end_0 - start_0 > 3
 
-        down_count = 0
-        while vm.clock_hz_exponent > 0 and down_count < 70:
-            down_count += 1
-            press_key(pygame.K_LEFTBRACKET)
+    for _ in range(2):
+        press_key(pygame.K_RIGHTBRACKET, True)
+    start_1 = vm.clock_count
+    sleep(0.125)
+    end_1 = vm.clock_count
 
-        assert 0 < down_count < 61
+    assert end_1 - start_1 > 2.125 * (end_0 - start_0)
 
-        press_key(pygame.K_LEFTBRACKET)
+    for _ in range(4):
+        press_key(pygame.K_LEFTBRACKET, True)
+    start_2 = vm.clock_count
+    sleep(0.125)
+    end_2 = vm.clock_count
+
+    assert end_2 - start_2 < 0.8 * (end_0 - start_0)
+
+    down_count = 0
+    while vm.clock_hz_exponent > 0 and down_count < 70:
+        down_count += 1
         press_key(pygame.K_LEFTBRACKET)
 
-        assert vm.clock_hz_exponent == 0
+    assert 0 < down_count < 61
 
-        up_count = 0
-        previous_value = -1
-        while vm.clock_hz_exponent != previous_value and up_count < 70:
-            previous_value = vm.clock_hz_exponent
-            up_count += 1
-            press_key(pygame.K_RIGHTBRACKET)
+    press_key(pygame.K_LEFTBRACKET)
+    press_key(pygame.K_LEFTBRACKET)
 
-        assert 65 > up_count > 9
+    assert vm.clock_hz_exponent == 0
 
-        press_key(pygame.K_p)
-        press_key(pygame.K_c)
+    up_count = 0
+    previous_value = -1
+    while vm.clock_hz_exponent != previous_value and up_count < 70:
+        previous_value = vm.clock_hz_exponent
+        up_count += 1
+        press_key(pygame.K_RIGHTBRACKET)
 
-        assert c.clock.value == 0
+    assert 65 > up_count > 9
 
-        enq(pygame.KEYDOWN, {"key": pygame.K_c})
 
-        assert c.clock.value == 1
+def test_clock_button(vm_enq_press: FixType) -> None:
+    vm, enq, press_key = vm_enq_press
 
-        enq(pygame.KEYDOWN, {"key": pygame.K_c})
+    press_key(pygame.K_c)
 
-        assert c.clock.value == 1
+    assert vm.computer.clock.value == 0
 
-        enq(pygame.KEYUP, {"key": pygame.K_c})
+    enq(pygame.KEYDOWN, {"key": pygame.K_c})
 
-        assert c.clock.value == 0
+    assert vm.computer.clock.value == 1
 
-        prev_twos = vm.twos
+    enq(pygame.KEYDOWN, {"key": pygame.K_c})
 
-        press_key(pygame.K_2)
+    assert vm.computer.clock.value == 1
 
-        assert vm.twos != prev_twos
+    enq(pygame.KEYUP, {"key": pygame.K_c})
 
-        press_key(pygame.K_2, True)
+    assert vm.computer.clock.value == 0
 
-        assert vm.twos == prev_twos
 
-        # just for the 100% coverage
-        vm.draw_box_for_registers = True
+def test_twos_complement_button(vm_enq_press: FixType) -> None:
+    vm, _, press_key = vm_enq_press
 
-        press_key(pygame.K_r)
-        # still paused
+    prev_twos = vm.twos
 
-        assert c.pc.value == 0
+    press_key(pygame.K_2)
 
-        # make sure holding down the reset button only resets it once
-        enq(pygame.KEYDOWN, {"key": pygame.K_r})
+    assert vm.twos != prev_twos
 
-        assert c.pc.value == 0
+    press_key(pygame.K_2, True)
 
-        c.pc.value = 14
-        enq(pygame.KEYDOWN, {"key": pygame.K_r})
+    assert vm.twos == prev_twos
 
-        assert c.pc.value == 14
 
-        assert vm.pause_message_timer == 0
+def test_reset(vm_enq_press: FixType) -> None:
+    vm, enq, press_key = vm_enq_press
 
-        press_key(pygame.K_o)
+    # just for the 100% coverage
+    vm.draw_box_for_registers = True
 
-        assert vm.pause_message_timer > 0
+    press_key(pygame.K_p)
+    press_key(pygame.K_p)
 
-    finally:
-        patched_pygame_event.enqueue(patched_pygame_event.Event(pygame.QUIT, {}))
-        run_thread.join(3)
+    assert vm.paused
+
+    press_key(pygame.K_r)
+
+    assert vm.computer.pc.value == 0
+
+    # make sure holding down the reset button only resets it once
+    enq(pygame.KEYDOWN, {"key": pygame.K_r})
+
+    assert vm.computer.pc.value == 0
+
+    vm.computer.pc.value = 14
+    enq(pygame.KEYDOWN, {"key": pygame.K_r})
+
+    assert vm.computer.pc.value == 14
+
+
+def test_pause_message(vm_enq_press: FixType) -> None:
+    vm, _, press_key = vm_enq_press
+
+    assert vm.pause_message_timer == 0
+
+    press_key(pygame.K_o)
+
+    assert vm.pause_message_timer > 0
